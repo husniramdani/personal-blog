94c4cc46bd013f6dfedda592e677a297dd520f70
husniramdani <husni.ramdani@codemi.co.id>
2022-01-02 01:15:18 +0700

    update component with data from notion and filtering some data

--- pages/index.js
+++ pages/index.js
@@ -8,90 +8,49 @@ import Link from 'next/link';
 import Navbar from "@components/navbar";
 import Footer from "@components/footer";
 
-
-const data = [
-  {
-    id: 1,
-    creator: "Husni Ramdani",
-    published: "30 November 2021",
-    avatar: "/images/avatar.png",
-    images: "",
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet tempor nunc maecenas cras ipsum, lorem massa lacus",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat pretium, mi sed id dui sed orci, tempor. Pellentesque egestas odio enim, accumsan, cursus. Fermentum in bibendum aliquet est viverra eu vitae in nibh. Leo, feugiat amet neque, quis. Amet, eget vulputate cursus in eu sit pulvinar et. Nibh at sem viverra pellentesque hac odio duis a. Urna vitae, at ac et rhoncus. Mauris sit accumsan vitae, nibh netus. In elementum pharetra in lacinia nibh. Non est eget egestas eu et purus amet. Vitae aliquam sit tincidunt pellentesque netus suspendisse vulputate. Dui justo, ac maecenas pharetra. ", // mandatory
-  },
-  {
-    id: 2,
-    creator: "",
-    published: "",
-    avatar: "",
-    images: "",
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet tempor nunc maecenas cras ipsum, lorem massa lacus",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat pretium, mi sed id dui sed orci, tempor. Pellentesque egestas odio enim, accumsan, cursus. Fermentum in bibendum aliquet est viverra eu vitae in nibh. Leo, feugiat amet neque, quis. Amet, eget vulputate cursus in eu sit pulvinar et. Nibh at sem viverra pellentesque hac odio duis a. Urna vitae, at ac et rhoncus. Mauris sit accumsan vitae, nibh netus. In elementum pharetra in lacinia nibh. Non est eget egestas eu et purus amet. Vitae aliquam sit tincidunt pellentesque netus suspendisse vulputate. Dui justo, ac maecenas pharetra. ",
-  },
-  {
-    id: 3,
-    creator: "Husni Ramdani",
-    published: "",
-    avatar: "/images/avatar.png",
-    images: "/images/photos.png",
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet tempor nunc maecenas cras ipsum, lorem massa lacus",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat pretium, mi sed id dui sed orci, tempor. Pellentesque egestas odio enim, accumsan, cursus. Fermentum in bibendum aliquet est viverra eu vitae in nibh. Leo, feugiat amet neque, quis. Amet, eget vulputate cursus in eu sit pulvinar et. Nibh at sem viverra pellentesque hac odio duis a. Urna vitae, at ac et rhoncus. Mauris sit accumsan vitae, nibh netus. In elementum pharetra in lacinia nibh. Non est eget egestas eu et purus amet. Vitae aliquam sit tincidunt pellentesque netus suspendisse vulputate. Dui justo, ac maecenas pharetra. ",
-  },
-  {
-    id: 4,
-    creator: "Husni Ramdani",
-    published: "30 November 2021",
-    avatar: "/images/avatar.png",
-    images: "",
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet tempor nunc maecenas cras ipsum, lorem massa lacus",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat pretium, mi sed id dui sed orci, tempor. Pellentesque egestas odio enim, accumsan, cursus. Fermentum in bibendum aliquet est viverra eu vitae in nibh. Leo, feugiat amet neque, quis. Amet, eget vulputate cursus in eu sit pulvinar et. Nibh at sem viverra pellentesque hac odio duis a. Urna vitae, at ac et rhoncus. Mauris sit accumsan vitae, nibh netus. In elementum pharetra in lacinia nibh. Non est eget egestas eu et purus amet. Vitae aliquam sit tincidunt pellentesque netus suspendisse vulputate. Dui justo, ac maecenas pharetra. ",
-  },
-  {
-    id: 5,
-    creator: "Husni Ramdani",
-    published: "",
-    avatar: "",
-    images: "/images/photos.png",
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet tempor nunc maecenas cras ipsum, lorem massa lacus",
-    detail: "",
-  },
-  {
-    id: 6,
-    creator: "Husni Ramdani",
-    published: "",
-    avatar: "",
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet tempor nunc maecenas cras ipsum, lorem massa lacus",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat pretium, mi sed id dui sed orci, tempor. Pellentesque egestas odio enim, accumsan, cursus. Fermentum in bibendum aliquet est viverra eu vitae in nibh. Leo, feugiat amet neque, quis. Amet, eget vulputate cursus in eu sit pulvinar et. Nibh at sem viverra pellentesque hac odio duis a. Urna vitae, at ac et rhoncus. Mauris sit accumsan vitae, nibh netus. In elementum pharetra in lacinia nibh. Non est eget egestas eu et purus amet. Vitae aliquam sit tincidunt pellentesque netus suspendisse vulputate. Dui justo, ac maecenas pharetra. ",
-  },
-  {
-    id: 7,
-    creator: "Husni Ramdani",
-    published: "30 November 2021",
-    avatar: "/images/avatar.png",
-    images: "",
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet tempor nunc maecenas cras ipsum, lorem massa lacus",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat pretium, mi sed id dui sed orci, tempor. Pellentesque egestas odio enim, accumsan, cursus. Fermentum in bibendum aliquet est viverra eu vitae in nibh. Leo, feugiat amet neque, quis. Amet, eget vulputate cursus in eu sit pulvinar et. Nibh at sem viverra pellentesque hac odio duis a. Urna vitae, at ac et rhoncus. Mauris sit accumsan vitae, nibh netus. In elementum pharetra in lacinia nibh. Non est eget egestas eu et purus amet. Vitae aliquam sit tincidunt pellentesque netus suspendisse vulputate. Dui justo, ac maecenas pharetra. ",
-  },
-  {
-    id: 8,
-    creator: "Husni Ramdani",
-    published: "30 November 2021",
-    avatar: "/images/avatar.png",
-    images: "/images/photos.png",
-    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet tempor nunc maecenas cras ipsum, lorem massa lacus",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat pretium, mi sed id dui sed orci, tempor. Pellentesque egestas odio enim, accumsan, cursus. Fermentum in bibendum aliquet est viverra eu vitae in nibh. Leo, feugiat amet neque, quis. Amet, eget vulputate cursus in eu sit pulvinar et. Nibh at sem viverra pellentesque hac odio duis a. Urna vitae, at ac et rhoncus. Mauris sit accumsan vitae, nibh netus. In elementum pharetra in lacinia nibh. Non est eget egestas eu et purus amet. Vitae aliquam sit tincidunt pellentesque netus suspendisse vulputate. Dui justo, ac maecenas pharetra. ",
-  },
-]
-
-export async function getStaticProps(){
+export async function getStaticProps() {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
   const DB = process.env.DB;
   const response = await notion.databases.query({
     database_id: DB,
+    filter: {
+      and: [
+        {
+          property: "Title",
+          text: {
+            is_not_empty: true,
+          },
+        },
+        {
+          property: "Status",
+          select: {
+            equals: "published",
+          },
+        },
+      ]
+    },
+    // sorts: [
+    //   {
+        // property: "Published",
+    //     timestamp: "created_time",
+    //     direction: "descending"
+    //   }
+    // ]
+  });
+  const articles = response.results.map(({ id, cover, properties: { Creator, Title, Subtitle, Published, Layout } }) => {
+    return {
+      id,
+      cover: cover ? cover?.type === "external" ? cover.external.url : cover.file.url : "",
+      Title: Title?.title[0]?.plain_text || "",
+      Creator: Creator?.people[0] || "",
+      Subtitle: Subtitle.rich_text,
+      Published: Published?.date?.start || "",
+      Layout: Layout?.select?.name || "type1",
+    }
   })
   return {
     props: {
-      results: response.results,
+      results: articles,
     }
   }
 }
@@ -102,7 +61,7 @@ export default function Home({ results }) {
 
   useEffect(() => {
     console.log(results);
-  });
+  }, [results]);
 
   const handleScroll = (e) => {
     let currentPos = scrollEl.current.scrollLeft
@@ -128,45 +87,60 @@ export default function Home({ results }) {
           className={`flex flex-wrap flex-col content-start h-full overflow-x-auto -mt-5 pt-5 pl-8 md:pl-24 md:-mt-8 md:pt-8`}
         >
           {
-            data.map((x, index) => {
-              const T = x.title, I = x.images, D = x.detail, A = x.avatar, C = x.creator, P = x.published;
+            results.map((data, index) => {
+              const ID = data.id;
+              const title = data.Title, cover = data.cover, subtitle = data.Subtitle;
+              const avatar = data?.Creator?.avatar_url || "";
+              const creator = data?.Creator?.name || "";
+              const published = data.Published, layout = data.Layout;
               return (
                 <div key={index} className={`w-68 mb-4 mr-5 pr-5 border-r border-gray-300 md:w-80`}>
-                  {I &&
-                    <div className={`w-full ${A ? 'mb-4' : 'mb-2'}`}>
-                      <img src={I} alt="thumbnail" className={`object-cover w-full ${A ? 'h-50' : 'h-34'}`} />
+                  {cover &&
+                    <div className={`w-full ${avatar ? 'mb-4' : 'mb-2'}`}>
+                      <img src={cover} alt="thumbnail" className={`object-cover w-full ${avatar ? 'h-50' : 'h-34'}`} />
                     </div>
                   }
-                  <Link href={`/blog/${x.id}`}>
+                  <Link href={`/blog/${ID}`}>
                     <h2
                       className={`
                         font-serif font-semibold tracking-tight text-3xl break-words
                         cursor-pointer underline-offset-2 decoration-2 decoration-orange
                         hover:underline
                         md:tracking-wide
-                        ${A ? 'line-clamp-3 mb-3' : 'line-clamp-2 mb-2'}
+                        ${avatar ? 'line-clamp-3 mb-3' : 'line-clamp-2 mb-2'}
                       `}
                     >
-                      {/* handle it with overflow-wrap: break-word*/}
-                      {/* aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa */}
-                      {T}
+                      {title}
                     </h2>
                   </Link>
-                  {D &&
-                    <p className={`text-sm text-justify line-clamp-3 ${A ? 'mb-3' : 'mb-2'} leading-relaxed`}>{D}</p>
+                  {subtitle &&
+                    <p className={`text-sm text-justify line-clamp-3 ${avatar ? 'mb-3' : 'mb-2'} leading-relaxed`}>
+                      {
+                        subtitle.map((content, index) => {
+                          return (
+                            <span
+                              key={index}
+                              className={`${content.annotations.underline ? 'underline' : ''}`}
+                            >
+                              {content.plain_text}
+                            </span>
+                          );
+                        })
+                      }
+                    </p>
                   }
-                  {A ?
+                  {layout === "type1" && avatar ?
                     <div className='flex items-center'>
                       <div className='bg-black p-1.5 overflow-hidden w-10 h-10 rounded-xl'>
-                        <img src={A} alt="avatar" />
+                        <img src={avatar} alt="avatar" />
                       </div>
                       <div className='ml-2'>
-                        <h6 className='text-sm font-semibold'>{C}</h6>
-                        <h6 className='text-xs'>{P}</h6>
+                        <h6 className='text-sm font-semibold'>{creator}</h6>
+                        <h6 className='text-xs'>{published}</h6>
                       </div>
                     </div>
                     :
-                    C && <h6 className='text-sm font-semibold'>By <span className='text-orange'>{C}</span></h6>
+                    creator && <h6 className='text-sm font-semibold'>By <span className='text-orange'>{creator}</span></h6>
                   }
                   <hr className='mt-4 border-gray-300' />
                 </div>
