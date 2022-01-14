36d6467de8d1dd97be86408294711e8e2ae5e655
husniramdani <husni.ramdani@codemi.co.id>
2022-01-14 23:33:16 +0700

    cleaning code

--- components/circle-button.js
+++ components/circle-button.js
@@ -2,12 +2,19 @@ import React from "react";
 
 export default function CircleButton({ link, children }) {
   return (
-    <div className="group rounded-center h-[2.8rem] w-[2.8rem] bg-black hover:bg-gradient-to-b hover:from-orange hover:to-purple dark:bg-gray-200">
+    <div className="
+      group rounded-center h-[2.8rem] w-[2.8rem] bg-black
+      hover:bg-gradient-to-b hover:from-orange hover:to-purple
+      dark:bg-gray-200
+    ">
       <a
         href={link}
         target="_blank"
         rel="noreferrer"
-        className="rounded-center bg-sand h-10 w-10 dark:bg-black"
+        className="
+          rounded-center bg-sand h-10 w-10
+          dark:bg-black
+        "
       >
         {children}
       </a>
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -8,37 +8,44 @@ import Head from 'next/head';
 import Navbar from "@components/navbar";
 import Footer from "@components/footer";
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
-const DB = process.env.DB;
-
 export default function Blog({ pages, blocks }) {
-  const { Creator, Title: title, Published: published, cover } = pages;
-  const avatar = Creator?.avatar_url || "";
-  const creator = Creator?.name || "";
+  const {
+    Creator: { avatar_url = "", name = "" },
+    Title, Published, cover
+  } = pages;
 
   return (
     <div className='min-h-screen py-5 md:py-10'>
       {/* seo */}
       <Head>
-        <title>{title}</title>
+        <title>{Title}</title>
         <meta name="description" content="Detail content Blog Spindyzel" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <Navbar />
       <main className='flex justify-center'>
         {/* content */}
-        <div className='mb-5 py-5 mx-8 w-full md:mx-24 md:max-w-screen-md'>
+        <div className='
+          mb-5 py-5 mx-8 w-full
+          md:mx-24 md:max-w-screen-md
+        '>
           {/* Blog Title */}
-          <h1 className='mb-5 text-center font-bold text-4xl leading-normal line-clamp-3 md:text-6xl md:leading-relaxed'>
-            {title}
+          <h1 className='
+            mb-5 text-center font-bold text-4xl leading-normal line-clamp-3
+            md:text-6xl md:leading-relaxed
+          '>
+            {Title}
           </h1>
           {/* publisher info */}
           <div className='flex items-center justify-center'>
-            {avatar &&
-              <div className='relative bg-black overflow-hidden w-10 h-10 rounded-center p-2 md:w-12 md:h-12 md:p-2.5'>
+            {avatar_url &&
+              <div className='
+                relative bg-black overflow-hidden w-10 h-10 rounded-center p-2
+                md:w-12 md:h-12 md:p-2.5
+              '>
                 <Image
-                  src={avatar}
+                  src={avatar_url}
                   alt='avatar'
                   layout='fill'
                   objectFit="contain"
@@ -46,11 +53,11 @@ export default function Blog({ pages, blocks }) {
                 />
               </div>
             }
-            {creator &&
+            {name &&
               <div className='ml-2'>
-                <h6 className='text-sm font-semibold text-orange'>{creator}</h6>
-                {published &&
-                  <h6 className='text-xs'>{moment(published).format("DD MMMM YYYY")}</h6>
+                <h6 className='text-sm font-semibold text-orange'>{name}</h6>
+                {Published &&
+                  <h6 className='text-xs'>{moment(Published).format("DD MMMM YYYY")}</h6>
                 }
               </div>
 
@@ -67,51 +74,71 @@ export default function Blog({ pages, blocks }) {
             <img
               src={cover}
               alt="thumbnail"
-              className={`object-cover w-full max-h-96 md:max-h-100 lg:max-h-120`}
+              className="
+                object-cover w-full max-h-96
+                md:max-h-100
+                lg:max-h-120
+              "
             />
           }
           {/* content article */}
-          <article className='py-5 mx-auto max-w-xs sm:max-w-screen-sm md:max-w-screen-md prose dark:prose-invert prose-hr:border-gray-400 break-words'>
+          <article
+            className='
+              py-5 mx-auto max-w-xs break-words
+              sm:max-w-screen-sm
+              md:max-w-screen-md prose
+              dark:prose-invert
+            prose-hr:border-gray-400
+            '
+          >
             {
               blocks.map((block, index) => {
-                const key = block[block.type];
+                const { type } = block;
+                const key = block[type];
                 const imgContent = key?.file?.url || "";
-                const content = Array.isArray(key.text) ? key.text.map(({plain_text}) => plain_text).join(' ') : "-"
-                if (block.type === "heading_1") {
-                  return <h1 key={index}>{content}</h1>
-                } else if (block.type === "heading_2") {
-                  return <h2 key={index}>{content}</h2>
-                } else if (block.type === "heading_3") {
-                  return <h3 key={index}>{content}</h3>
-                } else if (block.type === "bulleted_list_item") {
-                  return (
-                    <ul key={index} className='not-prose'>
-                      <li>{content}</li>
-                    </ul>
-                  )
-                } else if (block.type === "quote") {
-                  return <blockquote className='border-black dark:border-gray-300' key={index}>{content}</blockquote>
-                } else if (block.type === "divider") {
-                  return <hr key={index} />
-                } else if (block.type === "image") {
-                  return <img key={index} src={imgContent} />
-                } else if (block.type === "code") {
-                  return <code key={index}>{content}</code>
-                } else {
-                  return <p key={index}>{content}</p>
+                const content = Array.isArray(key.text) ? key.text.map(({ plain_text }) => plain_text).join(' ') : "-";
+
+                switch (type) {
+                  case "heading_1":
+                    return <h1 key={index}>{content}</h1>
+                  case "heading_2":
+                    return <h2 key={index}>{content}</h2>
+                  case "heading_3":
+                    return <h3 key={index}>{content}</h3>
+                  case "bulleted_list_item":
+                    return (
+                      <ul key={index} className='not-prose'>
+                        <li>{content}</li>
+                      </ul>
+                    )
+                  case "quote":
+                    return (
+                      <blockquote className='border-black dark:border-gray-300' key={index}>
+                        {content}
+                      </blockquote>
+                    )
+                  case "divider":
+                    return <hr key={index} />
+                  case "image":
+                    return <img key={index} src={imgContent} />
+                  case "code":
+                    return <code key={index}>{content}</code>
+                  default:
+                    return <p key={index}>{content}</p>
                 }
               })
             }
           </article>
-          {/* default thumbnail */}
         </div>
-        {/* Empty Content */}
       </main>
 
       <Footer />
     </div>
   )
 }
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const DB = process.env.DB;
 
 export async function getStaticPaths() {
   const response = await notion.databases.query({
@@ -144,15 +171,24 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const BLOCK_ID = context.params.id;
-
   const pages = await notion.pages.retrieve({
     page_id: BLOCK_ID,
   });
-  const { id, cover, properties: { Creator, Title, Subtitle, Published, Layout } } = pages;
-
   const blocks = await notion.blocks.children.list({
     block_id: BLOCK_ID,
   });
+
+  const {
+    id,
+    cover,
+    properties: {
+      Creator,
+      Title,
+      Subtitle,
+      Published,
+      Layout
+    }
+  } = pages;
 
   return {
     props: {
